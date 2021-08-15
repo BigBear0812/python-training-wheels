1871da745bcdaa08d89e43342c00f001a3621c7c
Matthew Miller <miller@project-miller.com>
2021-08-15 15:18:07 -0400

    Create initial version of file converter service

--- src/pages/file-conveter/file-converter-sevice.ts
+++ src/pages/file-conveter/file-converter-sevice.ts
@@ -1,1 +1,21 @@
-export class FileConverterService{}
+import {HttpClient} from 'aurelia-http-client';
+import {autoinject} from 'aurelia-framework';
+
+@autoinject
+export class FileConverterService{
+  constructor(private httpClient: HttpClient){}
+
+  convertFile(file: File): Promise<File>{
+    let formData = new FormData();
+
+    formData.append("file", file);
+
+    return this.http.post("/api/ConvertFile", formData)
+      .then(response => {
+        return response.file;
+      });
+      then(file => {
+        return file;
+      })
+  }
+}
